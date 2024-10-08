5b620cec995edb58797b3a8a91fa4a48a7f0e805
Harikrishna <harikrishna.gurram@walmart.com>
2024-10-08 10:34:42 +0530

    React Router Hello World Application

---
+++ react/react-router/router-hello-world/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+// Create the routes using createBrowserRouter
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "about",
+    element: <About />,
+  },
+  {
+    path: "contact",
+    element: <Contact />,
+  },
+]);
+
+export default function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
