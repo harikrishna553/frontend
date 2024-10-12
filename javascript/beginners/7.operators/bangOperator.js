e18732f850b27fffe159d569b0ded3c586225c8e
Harikrishna <harikrishna.gurram@walmart.com>
2024-10-12 23:58:58 +0530

    Double bang operator

---
+++ javascript/beginners/7.operators/bangOperator.js
@@ -0,0 +1,11 @@
+let value1 = "Hello, World!";
+let value2 = 0;
+let value3 = null;
+let value4 = {};
+let value5 = NaN;
+
+console.log(`value1 : ${value1}, Boolean Value : ${!!value1}`);
+console.log(`value2 : ${value2}, Boolean Value : ${!!value2}`);
+console.log(`value3 : ${value3}, Boolean Value : ${!!value3}`);
+console.log(`value4 : ${value4}, Boolean Value : ${!!value4}`);
+console.log(`value5 : ${value5}, Boolean Value : ${!!value5}`);
