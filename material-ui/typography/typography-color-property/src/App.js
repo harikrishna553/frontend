0c91ec7e098d46888b4a404f5ff56c609534edaf
Harikrishna <harikrishna.gurram@walmart.com>
2024-10-17 20:24:11 +0530

    Typography color property

---
+++ material-ui/typography/typography-color-property/src/App.js
@@ -0,0 +1,16 @@
+import {Typography} from "@mui/material"
+
+function App() {
+  return (
+   <>
+    <Typography color="initial">Initial Colored Text</Typography>
+    <Typography color="primary">Primary Colored Text</Typography>
+    <Typography color="secondary">Secondary Colored Text</Typography>
+    <Typography color="textPrimary">Primary Text Colored Text</Typography>
+    <Typography color="textSecondary">Secondary Text Colored Text</Typography>
+    <Typography color="error">Error Colored Text</Typography>
+   </>
+  );
+}
+
+export default App;
