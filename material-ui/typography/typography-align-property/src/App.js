979be41ee595f2d35f1bcd2b13be63a858a7fd07
Harikrishna <harikrishna.gurram@walmart.com>
2024-10-17 20:13:41 +0530

    Typography align property demo

---
+++ material-ui/typography/typography-align-property/src/App.js
@@ -0,0 +1,49 @@
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Typography variant="h6">Default one</Typography>
+      <Typography variant="body1" gutterBottom>
+        The Typography component in Material-UI is designed to handle text
+        styling and formatting in a consistent and flexible way. The align
+        property of the Typography component is used to control the alignment of
+        the text content within its container
+      </Typography>
+
+      <Typography variant="h6">Left Aligned</Typography>
+      <Typography variant="body1" align="left" gutterBottom>
+        The Typography component in Material-UI is designed to handle text
+        styling and formatting in a consistent and flexible way. The align
+        property of the Typography component is used to control the alignment of
+        the text content within its container
+      </Typography>
+
+      <Typography variant="h6">Right Aligned</Typography>
+      <Typography variant="body1" align="right" gutterBottom>
+        The Typography component in Material-UI is designed to handle text
+        styling and formatting in a consistent and flexible way. The align
+        property of the Typography component is used to control the alignment of
+        the text content within its container
+      </Typography>
+
+      <Typography variant="h6">Center Aligned</Typography>
+      <Typography variant="body1" align="center" gutterBottom>
+        The Typography component in Material-UI is designed to handle text
+        styling and formatting in a consistent and flexible way. The align
+        property of the Typography component is used to control the alignment of
+        the text content within its container
+      </Typography>
+
+      <Typography variant="h6">Jistified</Typography>
+      <Typography variant="body1" align="justify" gutterBottom>
+        The Typography component in Material-UI is designed to handle text
+        styling and formatting in a consistent and flexible way. The align
+        property of the Typography component is used to control the alignment of
+        the text content within its container
+      </Typography>
+    </>
+  );
+}
+
+export default App;
