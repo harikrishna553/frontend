1f99202dd0435d79132c71f970b2d000578e6e88
Harikrishna <harikrishna.gurram@walmart.com>
2024-10-17 19:58:06 +0530

    Typography component property demo

---
+++ material-ui/typography/typography-component-property/src/App.js
@@ -0,0 +1,17 @@
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Typography variant="body1" component="h1">
+        This renders a Heading element
+      </Typography>
+
+      <Typography variant="body1">
+        This renders as regular body1 variant
+      </Typography>
+    </>
+  );
+}
+
+export default App;
