32130f75135439f6d482ebdfe8bef511d932111e
Harikrishna <harikrishna.gurram@walmart.com>
2024-10-19 09:33:35 +0530

    Fix ButtonGroup Demo App

--- material-ui/buttons/button-group-demo/src/App.js
+++ material-ui/buttons/button-group-demo/src/App.js
@@ -2,146 +2,159 @@ import { Button, ButtonGroup, Stack, Typography } from "@mui/material";
 
 function App() {
   return (
-    <Stack
-      sx={{
-        margin: "20px",
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "flex-start",
-        gap: "100px",
-        columnGap: "100px",
-      }}
-    >
-      <Stack>
-        <Typography variant="h5" gutterBottom>
-          Sample Button Group
-        </Typography>
-        <ButtonGroup>
-          <Button>Previous</Button>
-          <Button>Home</Button>
-          <Button>Next</Button>
-        </ButtonGroup>
-      </Stack>
-
-      <Stack>
-        <Typography variant="h5" gutterBottom>
-          Variant Demo
-        </Typography>
-
-        <Stack
-          sx={{
-            marginLeft: "20px",
-          }}
-        >
-          <Typography variant="h6" gutterBottom>
-            Text variant
+    <>
+      {" "}
+      <Stack
+        sx={{
+          margin: "20px",
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "flex-start",
+          gap: "100px",
+          columnGap: "100px",
+        }}
+      >
+        <Stack>
+          <Typography variant="h5" gutterBottom>
+            Sample Button Group
           </Typography>
-          <ButtonGroup variant="text">
+          <ButtonGroup>
             <Button>Previous</Button>
             <Button>Home</Button>
             <Button>Next</Button>
           </ButtonGroup>
+        </Stack>
 
-          <Typography variant="h6" gutterBottom>
-            Contained variant
+        <Stack>
+          <Typography variant="h5" gutterBottom>
+            Variant Demo
           </Typography>
-          <ButtonGroup variant="contained">
+
+          <Stack
+            sx={{
+              marginLeft: "20px",
+            }}
+          >
+            <Typography variant="h6" gutterBottom>
+              Text variant
+            </Typography>
+            <ButtonGroup variant="text">
+              <Button>Previous</Button>
+              <Button>Home</Button>
+              <Button>Next</Button>
+            </ButtonGroup>
+
+            <Typography variant="h6" gutterBottom>
+              Contained variant
+            </Typography>
+            <ButtonGroup variant="contained">
+              <Button>Previous</Button>
+              <Button>Home</Button>
+              <Button>Next</Button>
+            </ButtonGroup>
+
+            <Typography variant="h6" gutterBottom>
+              Contained variant Disable Elevation
+            </Typography>
+            <ButtonGroup variant="contained" disableElevation>
+              <Button>Previous</Button>
+              <Button>Home</Button>
+              <Button>Next</Button>
+            </ButtonGroup>
+
+            <Typography variant="h6" gutterBottom>
+              Outlined variant
+            </Typography>
+            <ButtonGroup variant="outlined">
+              <Button>Previous</Button>
+              <Button>Home</Button>
+              <Button>Next</Button>
+            </ButtonGroup>
+          </Stack>
+        </Stack>
+
+        <Stack>
+          <Typography variant="h5" gutterBottom>
+            Set Secondary Color for the Button Group
+          </Typography>
+          <ButtonGroup color="secondary" variant="contained" disableElevation>
             <Button>Previous</Button>
             <Button>Home</Button>
             <Button>Next</Button>
           </ButtonGroup>
+        </Stack>
 
-          <Typography variant="h6" gutterBottom>
-            Contained variant Disable Elevation
+        <Stack>
+          <Typography variant="h5" gutterBottom>
+            Buttons By Size
           </Typography>
-          <ButtonGroup variant="contained" disableElevation>
+          <ButtonGroup
+            variant="contained"
+            size="small"
+            disableElevation
+            sx={{
+              marginBottom: 2,
+            }}
+          >
             <Button>Previous</Button>
             <Button>Home</Button>
             <Button>Next</Button>
           </ButtonGroup>
+          <ButtonGroup
+            variant="contained"
+            size="medium"
+            disableElevation
+            sx={{
+              marginBottom: 2,
+            }}
+          >
+            <Button>Previous</Button>
+            <Button>Home</Button>
+            <Button>Next</Button>
+          </ButtonGroup>
+          <ButtonGroup
+            variant="contained"
+            size="large"
+            disableElevation
+            sx={{
+              marginBottom: 2,
+            }}
+          >
+            <Button>Previous</Button>
+            <Button>Home</Button>
+            <Button>Next</Button>
+          </ButtonGroup>
+        </Stack>
 
-          <Typography variant="h6" gutterBottom>
-            Outlined variant
+        <Stack>
+          <Typography variant="h5" gutterBottom>
+            Vertical Orientation
           </Typography>
-          <ButtonGroup variant="outlined">
+          <ButtonGroup
+            orientation="vertical"
+            variant="outlined"
+            sx={{
+              width: 100,
+            }}
+          >
+            <Button>Previous</Button>
+            <Button>Home</Button>
+            <Button>Next</Button>
+          </ButtonGroup>
+        </Stack>
+
+        <Stack>
+          <Typography variant="h5" gutterBottom>
+            Disable buttons
+          </Typography>
+          <ButtonGroup disabled>
             <Button>Previous</Button>
             <Button>Home</Button>
             <Button>Next</Button>
           </ButtonGroup>
         </Stack>
       </Stack>
-
-      <Stack>
-        <Typography variant="h5" gutterBottom>
-          Set Secondary Color for the Button Group
-        </Typography>
-        <ButtonGroup color="secondary" variant="contained" disableElevation>
-          <Button>Previous</Button>
-          <Button>Home</Button>
-          <Button>Next</Button>
-        </ButtonGroup>
-      </Stack>
-
-      <Stack>
-        <Typography variant="h5" gutterBottom>
-          Buttons By Size
-        </Typography>
-        <ButtonGroup
-          variant="contained"
-          size="small"
-          disableElevation
-          sx={{
-            marginBottom: 2,
-          }}
-        >
-          <Button>Previous</Button>
-          <Button>Home</Button>
-          <Button>Next</Button>
-        </ButtonGroup>
-        <ButtonGroup
-          variant="contained"
-          size="medium"
-          disableElevation
-          sx={{
-            marginBottom: 2,
-          }}
-        >
-          <Button>Previous</Button>
-          <Button>Home</Button>
-          <Button>Next</Button>
-        </ButtonGroup>
-        <ButtonGroup
-          variant="contained"
-          size="large"
-          disableElevation
-          sx={{
-            marginBottom: 2,
-          }}
-        >
-          <Button>Previous</Button>
-          <Button>Home</Button>
-          <Button>Next</Button>
-        </ButtonGroup>
-      </Stack>
-
-      <Stack>
-        <Typography variant="h5" gutterBottom>
-          Vertical Orientation
-        </Typography>
-        <ButtonGroup
-          orientation="vertical"
-          variant="outlined"
-          sx={{
-            width: 100,
-          }}
-        >
-          <Button>Previous</Button>
-          <Button>Home</Button>
-          <Button>Next</Button>
-        </ButtonGroup>
-      </Stack>
-
       <Stack>
         <Typography variant="h5" gutterBottom>
           Occupy Full Width
@@ -152,18 +165,7 @@ function App() {
           <Button>Next</Button>
         </ButtonGroup>
       </Stack>
-
-      <Stack>
-        <Typography variant="h5" gutterBottom>
-          Disable buttons
-        </Typography>
-        <ButtonGroup disabled>
-          <Button>Previous</Button>
-          <Button>Home</Button>
-          <Button>Next</Button>
-        </ButtonGroup>
-      </Stack>
-    </Stack>
+    </>
   );
 }
 
