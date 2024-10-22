46432a3442324ce4d4db023b2da2fd792aaad031
Harikrishna <harikrishna.gurram@walmart.com>
2024-10-22 18:08:28 +0530

    Accordion Group Demo

---
+++ material-ui/accordion/accordion-group/src/App.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+  Box,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+function App() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: "white",
+        margin: 5,
+      }}
+    >
+      <Typography variant="h5" gutterBottom>
+        Accordion Group Demo
+      </Typography>
+      <Accordion
+        expanded={expanded === "panel1"}
+        onChange={handleChange("panel1")}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <Typography>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>Content for Accordion 1.</Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === "panel2"}
+        onChange={handleChange("panel2")}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          <Typography>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>Content for Accordion 2.</Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === "panel3"}
+        onChange={handleChange("panel3")}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+          <Typography>Accordion 3</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>Content for Accordion 3.</Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+  );
+}
+
+export default App;
