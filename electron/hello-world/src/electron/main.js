7c1d4ae1ac4a5b85559f9cd21f6dd501fdb88e82
Harikrishna <harikrishna.gurram@walmart.com>
2024-12-09 16:53:48 +0530

    load url

--- electron/hello-world/src/electron/main.js
+++ electron/hello-world/src/electron/main.js
@@ -5,4 +5,5 @@ app.on("ready", () => {
     const mainWindow = new BrowserWindow({});
 
     mainWindow.loadFile(path.join(app.getAppPath(), 'dist-react', 'index.html'));
+    //mainWindow.loadURL("https://self-learning-java-tutorial.blogspot.com/2019/07/maven-tutorial.html")
 });
