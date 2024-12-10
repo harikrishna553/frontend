6c0ded3ba2245891767ef8501da697bae59618b1
Harikrishna <harikrishna.gurram@walmart.com>
2024-12-10 14:42:41 +0530

    Electron Hello World Application

--- electron/hello-world/src/electron/main.js
+++ electron/hello-world/src/electron/main.js
@@ -4,6 +4,6 @@ import path from 'path'
 app.on("ready", () => {
     const mainWindow = new BrowserWindow({});
 
-    mainWindow.loadFile(path.join(app.getAppPath(), 'dist-react', 'index.html'));
-    //mainWindow.loadURL("https://self-learning-java-tutorial.blogspot.com/2019/07/maven-tutorial.html")
+    //mainWindow.loadFile(path.join(app.getAppPath(), 'dist-react', 'index.html'));
+    mainWindow.loadURL("https://gdp.walmart.com/gdp-connect")
 });
