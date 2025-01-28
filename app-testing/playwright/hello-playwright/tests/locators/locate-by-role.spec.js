a59a1656b7ea29ff232efc29ba2e7303ed68596a
Harikrishna <harikrishna.gurram@walmart.com>
2025-01-28 12:19:55 +0530

    Locate By Role

--- app-testing/playwright/hello-playwright/tests/locators/locate-by-role.spec.js
+++ app-testing/playwright/hello-playwright/tests/locators/locate-by-role.spec.js
@@ -1,5 +1,19 @@
 import { test, expect } from "@playwright/test";
 
-test('locate by role', async({page}) => {
+test("Locate By Role", async ({ page }) => {
+    test.slow();
     
-})
+  // Navigate to the application
+  await page.goto("https://opensource-demo.orangehrmlive.com");
+
+  // Locate and interact with input fields by their placeholders
+  await page.getByPlaceholder("Username").fill("Admin");
+  await page.getByPlaceholder("Password").fill("admin123");
+
+  // Interact with elements using roles and accessible names
+  await page.getByRole("button", { type: "submit" }).click();
+
+  // Verify that the user is logged in by checking for a specific text
+  const dashboard = page.getByText("My Actions");
+  await expect(dashboard).toBeVisible();
+});
