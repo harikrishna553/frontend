a96ec9151a0ddd61c60684f15461ffe23b580ad3
Harikrishna <harikrishna.gurram@walmart.com>
2025-01-28 12:00:42 +0530

    Placeholder demo

---
+++ app-testing/playwright/hello-playwright/tests/locators/locate-by-placeholder.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from "@playwright/test";
+
+test("Locate By Placeholder", async ({ page }) => {
+  // Navigate to the application
+  await page.goto("https://opensource-demo.orangehrmlive.com");
+
+  // Locate and interact with input fields by their placeholders
+  await page.getByPlaceholder("Username").fill("Admin");
+  await page.getByPlaceholder("Password").fill("admin123");
+
+  // Click the login button
+  await page.getByRole("button", { type: "submit" }).click();
+
+  // Verify that the user is logged in by checking for a specific text
+  const element = page.getByText("My Actions");
+  await expect(element).toBeVisible();
+});
