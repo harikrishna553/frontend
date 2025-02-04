5ec170717210fe417231e8d8539fe4e839718893
Harikrishna <harikrishna.gurram@walmart.com>
2025-02-04 12:13:45 +0530

    toHaveTitle assertion example

--- app-testing/playwright/hello-playwright/tests/assertions/to-have-title.spec.js
+++ app-testing/playwright/hello-playwright/tests/assertions/to-have-title.spec.js
@@ -1,7 +1,17 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('Test Page Title', async ({page}) => {
-    await page.goto('https://demoblaze.com/')
-    
-    await expect(page).toHaveTitle('STORE')
-})
+test("Test Page Title", async ({ page }) => {
+  await page.goto("https://demoblaze.com/");
+
+  await expect(page).toHaveTitle("STORE");
+});
+
+test("Test Dynamic Page Title with Regex", async ({ page }) => {
+  await page.goto("https://demoblaze.com/");
+  await expect(page).toHaveTitle(/ORE/);
+});
+
+test("Test Page Title with Custom Timeout", async ({ page }) => {
+  await page.goto("https://demoblaze.com/");
+  await expect(page).toHaveTitle("STORE", { timeout: 5000 });
+});
