478062bd1cfa3b36535db5238a6d738c3603632b
Harikrishna <harikrishna.gurram@walmart.com>
2025-09-04 21:19:37 +0530

    Module export and imports

---
+++ typescript/typescript-basics/modules/arithmetic-demo.ts
@@ -0,0 +1,4 @@
+import {multiply, divide} from "./arithmetic"
+
+console.log(`2 * 3 is ${multiply(2, 3)}`)
+console.log(`10 / 5 is ${divide(10, 5)}`)
---
+++ typescript/typescript-basics/modules/string-utils-demo.ts
@@ -0,0 +1,4 @@
+import { convertToUpper as toUpper, convertToLower as toLower } from './string-utils';
+
+console.log(toUpper("typescript")); 
+console.log(toLower("HELLO"));    
