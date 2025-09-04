e748d887521d4d58f9f130e23fceb698d15d569f
Harikrishna <harikrishna.gurram@walmart.com>
2025-09-04 20:27:28 +0530

    Modules Examples

---
+++ typescript/typescript-basics/modules/math.ts
@@ -0,0 +1,15 @@
+export function add(x: number, y: number): number {
+  return x + y;
+}
+
+export function sub(x: number, y: number): number {
+  return x - y;
+}
+
+export function div(x: number, y: number): number {
+  return x / y;
+}
+
+export function multiply(x: number, y: number): number {
+  return x * y;
+}
